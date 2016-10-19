bb8b9974ececf45f166d4731ae7ef8ee91cc6296
Miguel DSW <mdsw@cin.ufpe.br>
2016-10-19 01:26:08 -0300

    model loaded from a saved model with some epochs of training

---
+++ MLP-Loaded.py
@@ -0,0 +1,99 @@
+#################
+##LOADED MLP 
+##from a saved model
+
+
+from __future__ import print_function
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("./data/", one_hot=True)
+
+import tensorflow as tf
+
+# Parameters
+learning_rate = 0.001
+training_epochs = 10
+batch_size = 100
+display_step = 1
+
+# Network Parameters
+n_hidden_1 = 128 # 1st layer number of features
+n_hidden_2 = 128 # 2nd layer number of features
+n_input = 784 # MNIST data input (img shape: 28*28)
+n_classes = 10 # MNIST total classes (0-9 digits)
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+
+# Create MLP model
+def MLP(x, weights, biases):
+    # 1st Hidden layer with RELU activation
+    layer1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer1 = tf.nn.relu(layer1)
+    # 2nd Hidden layer with RELU activation
+    layer2 = tf.add(tf.matmul(layer1, weights['h2']), biases['b2'])
+    layer2 = tf.nn.relu(layer2)
+    # Output layer with linear activation
+    layerOut = tf.matmul(layer2, weights['out']) + biases['out']
+    return layerOut
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1]), name = 'h1'),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2]), name = 'h2'),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]), name = 'h-out')
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1]), name = 'b1'),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2]), name = 'b2'),
+    'out': tf.Variable(tf.random_normal([n_classes]), name = 'b-out')
+}
+
+# Construct model
+pred = MLP(x, weights, biases)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+#REMOVE# Initializing the variables
+#REMOVE#init = tf.initialize_all_variables()
+
+# 'saver' to load all the variables
+saver = tf.train.Saver()
+
+# Launch the graph
+with tf.Session() as sess:
+    #REMOVE#sess.run(init)
+
+    # Load variables from disk
+    saver.restore(sess, "./save/MLP.ckpt")
+    print("MLP Loaded!")
+
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(mnist.train.num_examples/batch_size)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+                                                          y: batch_y})
+            # Compute average loss
+            avg_cost += c / total_batch
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print("Epoch:", '%04d' % (epoch+1), "cost=", \
+                "{:.9f}".format(avg_cost))
+    print("Optimization Finished!")
+
+
+    # Test model
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    # Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
