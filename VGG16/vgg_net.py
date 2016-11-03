c12038870e62d50449ae956187cbd1ecacd49c5f
migueldsw <mdsw@cin.ufpe.br>
2016-11-03 01:58:07 +0000

    fix accuracy print

--- VGG16/vgg_net.py
+++ VGG16/vgg_net.py
@@ -68,10 +68,12 @@ network = regression(network, optimizer='rmsprop',
 # Training
 model = tflearn.DNN(network, checkpoint_path='model_vgg',
                     max_checkpoints=1, tensorboard_verbose=0)
-model.fit(X, Y, n_epoch=5, shuffle=True,
+model.fit(X, Y, n_epoch=2, shuffle=True,
           show_metric=True, batch_size=3, snapshot_step=500,
           snapshot_epoch=False, run_id='vgg_oxflowers17')
 
 # Evaluate accuracy.
 accuracy_score = model.evaluate(testX,testY,batch_size=3)
-print('Accuracy: {0:f}'.format(accuracy_score))
+#print('Accuracy: {0:f}'.format(accuracy_score))
+print('Accuracy')
+print(accuracy_score)
