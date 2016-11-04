80cd8e12d0dcae000b854ca93cdfed0f913ad720
Miguel DSW <mdsw@cin.ufpe.br>
2016-11-03 21:28:09 -0300

    vgg model adjusted for mnist input shape (28x28x1)

--- VGG16/vgg_net_mnist.py
+++ VGG16/vgg_net_mnist.py
@@ -21,7 +21,7 @@ from tflearn.layers.estimator import regression
 import tflearn.datasets.mnist as mnist
 X, Y, testX, testY = mnist.load_data(one_hot=True)
 # from dataset_helper import get_small_mnist
-# X, Y, testX, testY = get_small_mnist(10)
+# X, Y, testX, testY = get_small_mnist(10000)
 #
 X = X.reshape(len(X),28,28,1)
 testX = testX.reshape(len(testX),28,28,1)
@@ -30,32 +30,32 @@ print ("Dataset in use: train size= %d; test size= %d" %(len(X),len(testX)))
 # Building 'VGG Network'
 network = input_data(shape=[None, 28, 28, 1])
 
+network = conv_2d(network, 8, 1, activation='relu')
+network = conv_2d(network, 8, 1, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = conv_2d(network, 16, 1, activation='relu')
+network = conv_2d(network, 16, 1, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
+network = conv_2d(network, 32, 1, activation='relu')
+network = conv_2d(network, 32, 1, activation='relu')
+network = conv_2d(network, 32, 1, activation='relu')
+network = max_pool_2d(network, 2, strides=2)
+
 network = conv_2d(network, 64, 1, activation='relu')
 network = conv_2d(network, 64, 1, activation='relu')
+network = conv_2d(network, 64, 1, activation='relu')
 network = max_pool_2d(network, 2, strides=2)
 
-network = conv_2d(network, 128, 1, activation='relu')
-network = conv_2d(network, 128, 1, activation='relu')
+network = conv_2d(network, 64, 1, activation='relu')
+network = conv_2d(network, 64, 1, activation='relu')
+network = conv_2d(network, 64, 1, activation='relu')
 network = max_pool_2d(network, 2, strides=2)
 
-network = conv_2d(network, 256, 1, activation='relu')
-network = conv_2d(network, 256, 1, activation='relu')
-network = conv_2d(network, 256, 1, activation='relu')
-network = max_pool_2d(network, 2, strides=2)
-
-network = conv_2d(network, 512, 1, activation='relu')
-network = conv_2d(network, 512, 1, activation='relu')
-network = conv_2d(network, 512, 1, activation='relu')
-network = max_pool_2d(network, 2, strides=2)
-
-network = conv_2d(network, 512, 1, activation='relu')
-network = conv_2d(network, 512, 1, activation='relu')
-network = conv_2d(network, 512, 1, activation='relu')
-network = max_pool_2d(network, 2, strides=2)
-
-network = fully_connected(network, 4096, activation='relu')
+network = fully_connected(network, 512, activation='relu')
 network = dropout(network, 0.5)
-network = fully_connected(network, 4096, activation='relu')
+network = fully_connected(network, 512, activation='relu')
 network = dropout(network, 0.5)
 network = fully_connected(network, 10, activation='softmax')
 
@@ -64,11 +64,11 @@ network = regression(network, optimizer='rmsprop',
                      learning_rate=0.001)
 
 # Training
-model = tflearn.DNN(network, checkpoint_path='model_vgg',
+model = tflearn.DNN(network, checkpoint_path='model_vgg_2',
                     max_checkpoints=1, tensorboard_verbose=0)
-model.fit(X, Y, n_epoch=10, shuffle=True,
+model.fit(X, Y, n_epoch=100, shuffle=True,
           show_metric=True, batch_size=32, snapshot_step=500,
-          snapshot_epoch=True, run_id='vgg_mnist_full')
+          snapshot_epoch=True, run_id='vgg_mnist_full_2')
 
 # Evaluate accuracy.
 accuracy_score = model.evaluate(testX,testY,batch_size=32)
