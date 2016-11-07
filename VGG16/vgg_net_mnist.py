dc6ed6d6d4d58a15ebbcf0a8d3eb9fc2420b4042
Miguel DSW <mdsw@cin.ufpe.br>
2016-11-07 02:01:01 -0300

    evaluates vgg16 on mnist with RGB layers: shape = (Nx28x28x3)

--- VGG16/vgg_net_mnist.py
+++ VGG16/vgg_net_mnist.py
@@ -18,39 +18,40 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.estimator import regression
 
 # Data loading and preprocessing
-import tflearn.datasets.mnist as mnist
-X, Y, testX, testY = mnist.load_data(one_hot=True)
-# from dataset_helper import get_small_mnist
-# X, Y, testX, testY = get_small_mnist(10000)
+print ("Data loading and preprocessing...")
 #
-X = X.reshape(len(X),28,28,1)
-testX = testX.reshape(len(testX),28,28,1)
-print ("Dataset in use: train size= %d; test size= %d" %(len(X),len(testX)))
+from dataset_helper import get_mnist
+# X, Y, testX, testY = get_mnist(instances=10, rgb=True)
+X, Y, testX, testY = get_mnist(rgb=True)
+#
+# from dataset_helper import get_svhn
+# X, Y, testX, testY = get_svhn(instances=10, crop=True)
+#
 
 # Building 'VGG Network'
-network = input_data(shape=[None, 28, 28, 1])
+network = input_data(shape=[None, 28, 28, 3])
 
-network = conv_2d(network, 8, 1, activation='relu')
-network = conv_2d(network, 8, 1, activation='relu')
+network = conv_2d(network, 8, 3, activation='relu')
+network = conv_2d(network, 8, 3, activation='relu')
 network = max_pool_2d(network, 2, strides=2)
 
-network = conv_2d(network, 16, 1, activation='relu')
-network = conv_2d(network, 16, 1, activation='relu')
+network = conv_2d(network, 16, 3, activation='relu')
+network = conv_2d(network, 16, 3, activation='relu')
 network = max_pool_2d(network, 2, strides=2)
 
-network = conv_2d(network, 32, 1, activation='relu')
-network = conv_2d(network, 32, 1, activation='relu')
-network = conv_2d(network, 32, 1, activation='relu')
+network = conv_2d(network, 32, 3, activation='relu')
+network = conv_2d(network, 32, 3, activation='relu')
+network = conv_2d(network, 32, 3, activation='relu')
 network = max_pool_2d(network, 2, strides=2)
 
-network = conv_2d(network, 64, 1, activation='relu')
-network = conv_2d(network, 64, 1, activation='relu')
-network = conv_2d(network, 64, 1, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
 network = max_pool_2d(network, 2, strides=2)
 
-network = conv_2d(network, 64, 1, activation='relu')
-network = conv_2d(network, 64, 1, activation='relu')
-network = conv_2d(network, 64, 1, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
 network = max_pool_2d(network, 2, strides=2)
 
 network = fully_connected(network, 512, activation='relu')
@@ -63,12 +64,33 @@ network = regression(network, optimizer='rmsprop',
                      loss='categorical_crossentropy',
                      learning_rate=0.001)
 
+#-----------------
+#time cost evaluation
+from datetime import datetime as dt
+TIME = [] #[t_init,t_final]
+def startCrono():
+	TIME.append(dt.now())
+def getCrono(): # returns delta t in seconds
+	TIME.append(dt.now())
+	deltat = TIME[-1]-TIME[-2]
+	return deltat.seconds
+#----------------
+
+startCrono()
+
 # Training
-model = tflearn.DNN(network, checkpoint_path='model_vgg_2',
+print ("Training VGG-16...")
+EPOCHS = 50
+
+model = tflearn.DNN(network, checkpoint_path='model_vgg_3',
                     max_checkpoints=1, tensorboard_verbose=0)
-model.fit(X, Y, n_epoch=20, shuffle=True,
+model.fit(X, Y, n_epoch=EPOCHS, shuffle=True,
           show_metric=True, batch_size=32, snapshot_step=500,
-          snapshot_epoch=True, run_id='vgg_mnist_full_2',validation_set=0.1)
+          snapshot_epoch=True, run_id='vgg_mnist_full_3',validation_set=0.0)
+
+print ("Dataset in use: train size= %d; test size= %d" %(len(X),len(testX)))
+print ("Training completed in %d s"%(getCrono()))
+print ('Epochs: %d'%EPOCHS)
 
 # Evaluate accuracy.
 accuracy_score = model.evaluate(testX,testY,batch_size=32)
