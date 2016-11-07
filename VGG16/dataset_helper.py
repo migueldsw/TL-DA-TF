afb6b1286df2d6bc2e851d4720d8555999b7c14b
Miguel DSW <mdsw@cin.ufpe.br>
2016-11-06 22:33:53 -0300

    crop image data - (32x32x3)->(28x28x3)

--- VGG16/dataset_helper.py
+++ VGG16/dataset_helper.py
@@ -42,6 +42,15 @@ def get_small_mnist(instances):
 		tx, ty = testX[-3:], testY[-3:]
 	return x, y, tx, ty
 
+def get_small_svhn(instances):
+	import tflearn.datasets.svhn as svhn
+	X, Y, testX, testY = svhn.load_data(one_hot=True)
+	x, y = small_set(X,Y,instances,10)
+	tx, ty = small_set(testX,testY,instances/10,10)
+	if (len(tx)<1):
+		tx, ty = testX[-3:], testY[-3:]
+	return x, y, tx, ty
+
 def get_small_oxf17(instances):
 	import tflearn.datasets.oxflower17 as oxflower17
 	fX, fY = oxflower17.load_data(one_hot=True)
@@ -73,3 +82,19 @@ def include_RBG_in_set(X):
 	for i in range(len(X)):
 		out[i] = include_RGB_layers(X[i])
 	return out
+
+def crop_img(data):
+	#data shape: (32x32x3) crop to (28x28x3)
+	ndata = np.zeros(28*28*3).reshape(28,28,3)
+	for rgb in range(3):
+		for i in range(28):
+			for j in range(28):
+				ndata[i,j,rgb] = data[i+2,j+2,rgb]
+	return ndata
+
+def crop_images_in_set(X):
+	#crops images in set X from shape (32x32x3) to new shape(28x28x3)
+	nX = np.zeros(len(X)*28*28*3).reshape(len(X),28,28,3)
+	for i in range(len(X)):
+		nX[i] = crop_img(X[i])
+	return nX
