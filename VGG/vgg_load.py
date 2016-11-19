4e00854307cc993b16cf7b249d17c9866b59b742
Ubuntu <usuario@m1.0k11yg3yyfuefhtegupbbtxuld.bx.internal.cloudapp.net>
2016-11-19 18:06:08 +0000

    code ident fix

--- VGG/vgg_load.py
+++ VGG/vgg_load.py
@@ -11,7 +11,7 @@ from tflearn.layers.estimator import regression
 import os
 
 def vgg11(input, num_class, transf_params_encoded=['FT','FT','FT','FT','FT','FT','FT','FT','FT','FT','NT']):
-	transf_params = transfer_params_decode(transf_params_encoded)
+    transf_params = transfer_params_decode(transf_params_encoded)
 
     network = conv_2d(input, 8, 3, activation='relu', scope='conv1_1', restore=transf_params[0][0], trainable=transf_params[0][1])
     network = max_pool_2d(network, 2, strides=2, name='maxpool1')
@@ -42,7 +42,7 @@ def vgg11(input, num_class, transf_params_encoded=['FT','FT','FT','FT','FT','FT'
     return network
 
 def vgg16(input, num_class, ransf_params_encoded=['FT','FT','FT','FT','FT','FT','FT','FT','FT','FT','FT','FT','FT','FT','FT','NT']):
-	transf_params = transfer_params_decode(transf_params_encoded)
+    transf_params = transfer_params_decode(transf_params_encoded)
 
     network = conv_2d(input, 8, 3, activation='relu', scope='conv1_1', restore=transf_params[0][0], trainable=transf_params[0][1])
     network = conv_2d(network, 8, 3, activation='relu', scope='conv1_2', restore=transf_params[1][0], trainable=transf_params[1][1])
